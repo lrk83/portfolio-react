d4cab3824211c94d99755866cd3f4e10fe4dccad
Lukas Keel <lrk83@cornell.edu>
2021-10-19 12:04:21 -0400

    update About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -24,7 +24,7 @@ function About() {
           </Container>
           <div className="my-2">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+            Full Stack Web Developer with a background in the humanities and a unique perspective on tailoring the MERN stack to support creative and easy to use websites. Earned a certificate in Full Stack Web Development from the University of Texas Coding Boot Camp. Focussed on structuring back-end databases to smoothly serve end-user interactions, and designing mobile-first intuitive front-ends. A creative problem-solver with a conceptual orientation.
           </p>
           </div>
       </section>
